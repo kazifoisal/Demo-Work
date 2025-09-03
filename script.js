aea3b3907291e5040782f3ddf289759f6e6736b1
Kazi Foisal <108875654+kazifoisal@users.noreply.github.com>
2025-09-03 16:06:10 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+const startCountAnimation = (e) => {
+  let start = 0;
+  let end = parseInt(e.dataset.num);
+  let duration = 2000;
+  
+  let count = setInterval(() => {
+    start++;
+    e.textContent = start + "%";
+    if (start >= end) {
+      clearInterval(count);
+    }
+  }, duration / end);
+};
+
+const handleIntersection = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      startCountAnimation(entry.target);
+            observer.unobserve(entry.target);
+    }
+  });
+};
+
+const options = {
+  root: null, 
+  threshold: 0.5 
+};
+
+const observer = new IntersectionObserver(handleIntersection, options);
+
+const numElements = document.querySelectorAll(".num");
+
+numElements.forEach(element => {
+  observer.observe(element);
+});
